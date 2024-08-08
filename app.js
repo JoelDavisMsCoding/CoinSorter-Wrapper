9c8f49068cb3a07cd419a959b6c652b61c6d9e76
JoelDavisMsCoding <166549396+JoelDavisMsCoding@users.noreply.github.com>
2024-08-08 15:17:25 -0500

    Update
    I fixed the total amount to where it will round correctly if given a number longer than two decimals.

--- app.js
+++ app.js
@@ -5,45 +5,26 @@ let dimeWrapper = 0;
 let quarterWrapper = 0;
 
 //Variables for US Currency
-let hundredBill = 3; //This is saying I have 300 $100 dollar bills
-let fiftyBill = 3;
-let twentyBill = 3;
-let tenBill = 3;
-let fiveBill = 3;
-let dollarBill = 3;
 let quarter = 300;
 let dime = 300;
 let nickel = 300;
 let penny = 300;
 
 //Total Dollar amount for each Coin
-let hundredTotal = hundredBill * 100.00;
-let fiftyTotal = fiftyBill * 50.00;
-let twentyTotal = twentyBill * 20.00;
-let tenTotal = tenBill * 10.00;
-let fiveTotal = fiveBill * 5.00;
-let dollarTotal = dollarBill * 1.00;
 let quarterTotal = quarter * .25;
 let dimeTotal = dime * .10;
 let nickelTotal = nickel * .05;
 let pennyTotal = penny * .01;
 
 //Total of US currency total amount individually.
-console.log(`Hundred Dollar Bill: $${hundredTotal}`);
-console.log(`Fifty Dollar Bill: $${fiftyTotal}`);
-console.log(`Twenty Dollar Bill: $${twentyTotal}`);
-console.log(`Ten Dollar Bill: $${tenTotal}`);
-console.log(`Five Dollar Bill: $${fiveTotal}`);
-console.log(`Dollar Bill: $${dollarTotal}`);
 console.log(`Quarter: $${quarterTotal}`);
 console.log(`Dime: $${dimeTotal}`);
 console.log(`Nickel: $${nickelTotal}`);
 console.log(`Penny: $${pennyTotal}`);
 
 //Combined Total of all Coins
-let combinedCurrency = hundredTotal + fiftyTotal + twentyTotal + tenTotal + fiveTotal + dollarTotal + quarterTotal +
-dimeTotal + nickelTotal + pennyTotal;
-console.log(`Total Currency: $${combinedCurrency}`);
+let combinedCurrency = quarterTotal + dimeTotal + nickelTotal + pennyTotal;
+console.log(`Total Currency: $${combinedCurrency = Math.round(combinedCurrency * 100) / 100}`);
 console.log("");
 
 //Coins Per Wrapper for PENNIES
